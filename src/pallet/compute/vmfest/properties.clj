76db023b09f72febbbdeab99c67d6a73b0a0f3a3
Hugo Duncan <hugo@hugoduncan.org>
2013-02-14 09:30:40 -0500

    Correct vbox.home processing when property set
    An existing value for the property was being treated as a file.  Also adds
    logging of the value used and it's provenance.

--- src/pallet/compute/vmfest/properties.clj
+++ src/pallet/compute/vmfest/properties.clj
@@ -1,7 +1,8 @@
 (ns pallet.compute.vmfest.properties
   "Namespace to guess the vbox.home system property"
   (:use
-   [clojure.java.io :only [file]]))
+   [clojure.java.io :only [file]]
+   [clojure.tools.logging :only [debugf warnf]]))
 
 ;;; This has to be loaded before the vbox libs are initialised
 
@@ -12,5 +13,14 @@
 (defn guess-vbox-home []
   (first (filter #(.isDirectory %) (map file vbox-install-locations))))
 
-(when-let [vbox-home (or (System/getProperty "vbox.home") (guess-vbox-home))]
-  (System/setProperty "vbox.home" (.getAbsolutePath vbox-home)))
+(if-let [vbox-home (System/getProperty "vbox.home")]
+  (if (.isDirectory (file vbox-home))
+    (debugf "The vbox.home system property supplied as %s" vbox-home)
+    (warnf
+     "The vbox.home system property is set to %s, but that path does not exist."
+     vbox-home))
+  (when-let [vbox-home (guess-vbox-home)]
+    (debugf
+     "The vbox.home system property guessed as %s"
+     (.getAbsolutePath vbox-home))
+    (System/setProperty "vbox.home" (.getAbsolutePath vbox-home))))
