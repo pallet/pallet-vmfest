6557d9797a1c488674b6ae3a14ce059252c5ee0a
Antoni Batchelli <tbatchelli@acm.org>
2013-05-09 20:58:36 -0700

    Update vmfest to 0.3.0-alpha.5.

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/pallet/pallet-vmfest"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[vmfest "0.3.0-alpha.4"]
+  :dependencies [[vmfest "0.3.0-alpha.5"]
                  [org.tcrawley/dynapath "0.2.3"]])
