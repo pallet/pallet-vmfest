f1a738e6b0eee5400e665345c6ce843c6d7c69a2
Hugo Duncan <hugo@hugoduncan.org>
2013-10-07 12:08:39 -0400

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.palletops/pallet-vmfest "0.3.0-beta.2"
+(defproject com.palletops/pallet-vmfest "0.3.0-SNAPSHOT"
   :description "A pallet provider for using vmfest."
   :url "https://github.com/pallet/pallet-vmfest"
   :license {:name "Eclipse Public License"
