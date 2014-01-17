eb40a3f6f915f257ef5af70056c96ed962b7c823
Antoni Batchelli <tbatchelli@acm.org>
2014-01-17 11:56:46 -0800

    Use vmfest-0.4.0-alpha.1

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/pallet/pallet-vmfest"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[vmfest "0.4.0-SNAPSHOT"]
+  :dependencies [[vmfest "0.4.0-alpha.1"]
                  [org.tcrawley/dynapath "0.2.3"]])
